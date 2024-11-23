068ba35a2efaa9d8c58abf2849aa5eabebe36c76
pranjalpandey693 <pranjalpandey693@gmail.com>
2024-11-23 23:16:58 +0530

    fixed cors issue

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const schoolRoute = require('./route/schoolRoutes')
 const app = express();
 const cors = require("cors")
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+}))
 
 app.use(express.json());
 
